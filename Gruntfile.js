7783f96aa9515d3dd6bf67eb881b1e1459378419
CExBBartolome <bbartolome@i-sight.com>
2014-11-29 13:27:16 -0500

    Added analysis task to Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,12 +4,27 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     // Task configuration.
-
+	open: {
+	      platoReport: {
+		      path: './plato/index.html',
+		      app: 'Google Chrome'
+		    }
+	    },
+        plato: {
+	      src: {
+	              options : {
+		                jshint : grunt.file.readJSON('.jshintrc')
+		              },
+	              files: {
+		                'plato': ['mynosql.js']
+		              }
+	            }
+	    }
   });
 
   // These plugins provide necessary tasks.
-
+  require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
   // Default task.
   grunt.registerTask('default', []);
-
+  grunt.registerTask('analysis', ['plato:src', 'open:platoReport']);
 };
