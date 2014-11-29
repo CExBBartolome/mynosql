d2623ca0d38fc8b2cfd78f75fa43a792c5525c40
CExBBartolome <bbartolome@i-sight.com>
2014-11-29 00:35:31 -0500

    Added Gruntfile.js - grunt-init gruntfile - removed default tasks

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+
+  });
+
+  // These plugins provide necessary tasks.
+
+  // Default task.
+  grunt.registerTask('default', []);
+
+};
